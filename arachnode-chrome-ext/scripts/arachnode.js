5dfc46a97e79e6b1530134b5045850fc0a928777
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2023-08-28 15:45:52 -0500

    Adds ability to scrape for hover and other state attributes

--- arachnode-chrome-ext/scripts/arachnode.js
+++ arachnode-chrome-ext/scripts/arachnode.js
@@ -78,6 +78,23 @@ function getAttributes(elem, relationship, depth = undefined, onlyTag=false) {
         var computedValue = $(elem).css(property);
         if (computedValue != $('#arachnode').css(property) && !property.startsWith("-webkit")) attrs[property] = computedValue;
     }
+    const sheets = document.styleSheets;
+    Array.from(sheets).forEach(sheet => {
+        Array.from(sheet.cssRules || []).forEach(rule => {
+            if (rule.selectorText && rule.selectorText.includes(":")) {
+                if (elem.matches(rule.selectorText)) {
+                    //hoverStyles = rule.style;
+                    for (let i = 0; i < rule.style.length; i++) {
+                        const propName = rule.style[i];
+                        const propValue = rule.style.getPropertyValue(propName);
+                        if (propValue != "") {
+                            attrs[propName + ":" + rule.selectorText.split(":")[1]] = propValue;
+                        }
+                    }
+                }
+            }
+        });
+    });
     $('#arachnode').remove();
     $.each(elem.attributes, function () {
         if (this.specified) {
